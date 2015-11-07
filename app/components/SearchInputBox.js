47a3fc9ceaa3849cfebb76dbf95072720f2dc740
Armand <apaquino@gmail.com>
2015-11-06 17:55:17 -0800

    more es2015

--- app/components/SearchInputBox.js
+++ app/components/SearchInputBox.js
@@ -14,19 +14,19 @@ let {
 class SearchInputBox extends Component {
   render() {
     return (
-        <TextInput
-          ref="searchTextInput"
-          style={styles.textInput}
-          placeholder="  Search for something"
-          autoFocus={true}
-          autoCorrect={false}
-          returnKeyType="search"
-          enablesReturnKeyAutomatically={true}
-          autoCapitalize="none"
-          onSubmitEditing={this.props.handleOnSubmitEditing}
-          clearButtonMode='always'
-          clearTextOnFocus={true}
-        />
+      <TextInput
+        ref="searchTextInput"
+        style={styles.textInput}
+        placeholder="  Search for something"
+        autoFocus={true}
+        autoCorrect={false}
+        returnKeyType="search"
+        enablesReturnKeyAutomatically={true}
+        autoCapitalize="none"
+        onSubmitEditing={this.props.handleOnSubmitEditing}
+        clearButtonMode='always'
+        clearTextOnFocus={true}
+      />
     );
   }
 };
