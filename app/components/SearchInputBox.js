10481dbb17f4f655f7a232d9e4289dfe0e0af9fe
Armand <apaquino@gmail.com>
2015-12-27 16:54:01 -0800

    add paddingLeft to search input

--- app/components/SearchInputBox.js
+++ app/components/SearchInputBox.js
@@ -40,7 +40,8 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
     backgroundColor: "white",
-    borderRadius: 10
+    borderRadius: 10,
+    paddingLeft: 8
   }
 });
 
